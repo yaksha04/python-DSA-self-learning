b8e9c22e0257613a7ca184fe60bf7bd1f10a43bb
yaksha04 <yakshatikoria04@gmail.com>
2026-01-24 22:33:08 +0530

    Implement function to find max sum of subarray of size k

---
+++ array/Maximum_sum_subarray_of_size_k.py
@@ -0,0 +1,9 @@
+def max_sum_k(arr, k):
+    window_sum = sum(arr[:k])
+    max_sum = window_sum
+
+    for i in range(k, len(arr)):
+        window_sum += arr[i] - arr[i-k]
+        max_sum = max(max_sum, window_sum)
+
+    return max_sum
