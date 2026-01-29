937b263343a81c8973687fdc3eea17f0f94dfe2f
yaksha04 <yakshatikoria04@gmail.com>
2026-01-29 21:58:08 +0530

    Implement trapping rain water algorithm

---
+++ array/trapping_rain_water.py
@@ -0,0 +1,15 @@
+def trap(height):
+    left, right = 0, len(height) - 1
+    left_max = right_max = 0
+    water = 0
+
+    while left < right:
+        if height[left] < height[right]:
+            left_max = max(left_max, height[left])
+            water += left_max - height[left]
+            left += 1
+        else:
+            right_max = max(right_max, height[right])
+            water += right_max - height[right]
+            right -= 1
+    return water
