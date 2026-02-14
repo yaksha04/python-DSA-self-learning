b1df32cf868f0f34504443642e4e4a14d3d244c1
yaksha04 <yakshatikoria04@gmail.com>
2026-02-14 23:30:29 +0530

    Implement function to validate parentheses

---
+++ stack/valid_paranthesis.py
@@ -0,0 +1,13 @@
+def isValid(s):
+    stack = []
+    mapping = {')': '(', '}': '{', ']': '['}
+
+    for char in s:
+        if char in mapping:
+            if not stack or stack[-1] != mapping[char]:
+                return False
+            stack.pop()
+        else:
+            stack.append(char)
+
+    return not stack
