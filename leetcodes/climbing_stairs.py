fab0bc2d5219e73f499bdcf1fe524ad6fcf6e78e
yaksha04 <yakshatikoria04@gmail.com>
2026-02-17 23:24:50 +0530

    climbing stairs dynamic programming basic

---
+++ leetcodes/climbing_stairs.py
@@ -0,0 +1,12 @@
+def climbStairs(n):
+    if n <= 2:
+        return n
+
+    prev1, prev2 = 2, 1
+
+    for i in range(3, n + 1):
+        current = prev1 + prev2
+        prev2 = prev1
+        prev1 = current
+
+    return prev1
