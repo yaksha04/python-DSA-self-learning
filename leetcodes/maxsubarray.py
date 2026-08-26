ffcec20e3190b1869d878fee5747fcdf1f8cf26c
YAKSHA <yakshatikoria04@gmail.com>
2026-08-26 16:32:59 +0530

    Create maxsubarray.py

---
+++ leetcodes/maxsubarray.py
@@ -0,0 +1,11 @@
+class Solution:
+    def maxSubArray(self, nums: List[int]) -> int:
+        current_sum=0
+        max_sum=nums[0]
+        for i in range(len(nums)):
+            current_sum+=nums[i]
+            if current_sum>max_sum:
+                 max_sum=current_sum
+            if current_sum<0:
+                 current_sum=0
+        return max_sum            
